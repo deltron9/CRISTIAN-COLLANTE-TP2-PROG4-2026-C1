2a5a2dbb8116d7dc36483929db6de2225e8207f7
deltron9 <cristian_collante@hotmail.com>
2026-05-24 02:28:09 -0300

    add: rutas iniciales para sprint 1

--- front-utnials/src/app/app.routes.ts
+++ front-utnials/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Register } from './auth/register/register';
+import { Login } from './auth/login/login';
+import { ErrorPage } from './pages/error-page/error-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: Login
+            },
+            {
+                path: 'register',
+                component: Register
+            }
+        ]
+    },
+    {
+        path: 'mi-perfil',
+        loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((a) => a.MiPerfil),
+    },
+    {
+        path: 'publicaciones',
+        loadComponent: () => import('./pages/publicaciones/publicaciones').then((a) => a.Publicaciones)
+    },
+    {
+        path: 'error-page',
+        component: ErrorPage
+    },
+    {
+        path: '**',
+        redirectTo: 'error-page'
+    }
+];
