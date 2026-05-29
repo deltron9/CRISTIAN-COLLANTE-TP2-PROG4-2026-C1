59f0d8431321aba8d344b04f66d1ec0c74bab2da
deltron9 <cristian_collante@hotmail.com>
2026-05-29 03:57:17 -0300

    add: modulos de sprint 1

---
+++ back-utnials/src/publicaciones/publicaciones.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PublicacionesService } from './publicaciones.service';
+import { PublicacionesController } from './publicaciones.controller';
+
+@Module({
+  controllers: [PublicacionesController],
+  providers: [PublicacionesService],
+  exports: [PublicacionesService]
+})
+export class PublicacionesModule {}
