20dcc61efa182f16f28cb8b4c060cdc72e16649f
deltron9 <cristian_collante@hotmail.com>
2026-06-12 04:43:07 -0300

    add: interfaz para formData

---
+++ front-utnials/src/app/auth/iregistrar.ts
@@ -0,0 +1,10 @@
+export interface Iregistrar {
+    nombre: string;
+    apellido: string;
+    email: string;
+    username: string;
+    password: string;
+    fechaNacimiento: string;
+    descripcion: string;
+    fotoPerfil: File;
+}
