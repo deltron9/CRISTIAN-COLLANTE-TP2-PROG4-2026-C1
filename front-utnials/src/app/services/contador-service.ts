a9e1dfe5e065039d682f879950c88924629e3381
deltron9 <cristian_collante@hotmail.com>
2026-06-17 14:45:42 -0300

    add: servicio de contador controlar tiempo de sesion

---
+++ front-utnials/src/app/services/contador-service.ts
@@ -0,0 +1,57 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { AlertService } from './alert-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContadorService {
+
+  alert = inject(AlertService);
+  sesionExpirada = signal<boolean>(false);
+  timerAdvertencia: any;
+  timerExpiracionFinal: any;
+
+  iniciarContadores() {
+    this.limpiarTimers();
+    this.sesionExpirada.set(false);
+
+    const TIEMPO_ADVERTENCIA = 10 * 60 * 1000; 
+
+    this.timerAdvertencia = setTimeout(async () => {
+      
+      const TIEMPO_DESPUES_ADVERTENCIA = 5 * 60 * 1000; 
+      this.timerExpiracionFinal = setTimeout(() => {
+
+        this.sesionExpirada.set(true); 
+        this.limpiarTimers();
+      }, TIEMPO_DESPUES_ADVERTENCIA);
+
+      const quiereExtender = await this.alert.msjExpiracionSesion();
+
+      if (quiereExtender) {
+        this.notificarExtenderSesion();
+      } else {
+
+        this.sesionExpirada.set(true);
+        this.limpiarTimers();
+      }
+
+    }, TIEMPO_ADVERTENCIA);
+  }
+
+  reseteadoExitoso() {
+    this.limpiarTimers();
+  }
+
+  limpiarTimers() {
+    clearTimeout(this.timerAdvertencia);
+    clearTimeout(this.timerExpiracionFinal);
+  }
+
+  notificarExtenderSesion() {
+    this.limpiarTimers();
+    if (window) {
+      window.dispatchEvent(new CustomEvent('extender_sesion_click'));
+    }
+  }
+}
