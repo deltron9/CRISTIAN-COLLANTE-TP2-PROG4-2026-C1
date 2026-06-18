64529a7de5414268193a734991d72780fcfc4165
deltron9 <cristian_collante@hotmail.com>
2026-06-18 15:24:17 -0300

    feat: navbar lateral estando logeado

--- front-utnials/src/app/shared/navbar/navbar.ts
+++ front-utnials/src/app/shared/navbar/navbar.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth-service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  auth = inject(AuthService);
+}
