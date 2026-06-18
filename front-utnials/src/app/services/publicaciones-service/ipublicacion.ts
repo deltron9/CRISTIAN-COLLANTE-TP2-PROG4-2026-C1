a30b80f518b93200eaffb8803743c5d9ea212d72
deltron9 <cristian_collante@hotmail.com>
2026-06-18 18:36:33 -0300

    add: interface para publicaciones

---
+++ front-utnials/src/app/services/publicaciones-service/ipublicacion.ts
@@ -0,0 +1,24 @@
+export interface Publicacion {
+    _id: string;
+    titulo: string;
+    descripcion: string;
+    imagenUrl?: string;
+  
+    autor: {
+        _id: string;
+        username: string;
+        nombre: string;
+        apellido: string;
+        imagen: string;
+    };
+
+    likes: string[];
+    likesCantidad: number;
+
+    comentarios: string[];
+    comentariosCantidad: number;
+
+    activo: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
