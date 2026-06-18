d122969ee99698a90cf5d89589d6e90e287a1351
deltron9 <cristian_collante@hotmail.com>
2026-06-18 14:50:07 -0300

    add: logica para logeo

--- front-utnials/src/app/auth/login/login.ts
+++ front-utnials/src/app/auth/login/login.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth-service';
+import { AlertService } from '../../services/alert-service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,11 +13,25 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   styleUrls: ['./login.css']
 })
 export class Login {
-    formLogin = new FormGroup({
-    usuario: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(15), Validators.pattern('^[a-zA-Z0-9_\\-\\.\\@]+$')]),
-    password: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]*$'), Validators.minLength(8), Validators.maxLength(20)])
-  });
+  auth = inject(AuthService);
+  alert = inject(AlertService);
+  router = inject(Router);
 
-  sendForm() {
+  formLogin = new FormGroup({
+    usuario: new FormControl('', [
+      Validators.required, Validators.minLength(4), Validators.maxLength(50), 
+      Validators.pattern('^[a-zA-Z0-9_\\-\\.\\@\\+]+$')]),
+      
+    password: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]*$'), 
+      Validators.minLength(8), Validators.maxLength(20)])});
+
+  async sendForm() {
+    if (this.formLogin.invalid) {
+      this.alert.msjWarning('Por favor, revisa los campos ingresados');
+      return;
+    }
+
+    const credenciales = {identificador: this.formLogin.value.usuario!, passwordIngresada: this.formLogin.value.password!};
+    await this.auth.logear(credenciales);
   }
 }
