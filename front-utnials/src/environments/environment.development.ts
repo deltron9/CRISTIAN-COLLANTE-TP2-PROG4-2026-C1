7abbc95d8e5e8e07ea08eb7cf114074af0c94819
deltron9 <cristian_collante@hotmail.com>
2026-06-19 04:38:38 -0300

    add: variables

---
+++ front-utnials/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    NG_APP_BACKEND_URL: 'http://localhost:3030'
+};
---
+++ front-utnials/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    NG_APP_BACKEND_URL: ''
+};
