7e6ba2e681600557d503ed2db127a363238f32b3
deltron9 <cristian_collante@hotmail.com>
2026-06-20 04:11:43 -0300

    chore: cambio de validacion de caracteres a 500

--- back-utnials/src/comentarios/dto/update-comentario.dto.ts
+++ back-utnials/src/comentarios/dto/update-comentario.dto.ts
@@ -3,6 +3,6 @@ import { IsString, MinLength, MaxLength } from 'class-validator';
 export class UpdateComentarioDto {
     @IsString()
     @MinLength(1, { message: 'El comentario no puede estar vacío' })
-    @MaxLength(300, { message: 'El comentario no puede exceder los 300 caracteres' })
-    texto: string;
+    @MaxLength(500, { message: 'El comentario no puede exceder los 500 caracteres' })
+    texto!: string;
 }
