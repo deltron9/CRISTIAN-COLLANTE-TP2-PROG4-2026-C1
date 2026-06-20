4c4701febc6dc727320696cb01ee4ff4d3f04037
deltron9 <cristian_collante@hotmail.com>
2026-06-20 03:30:32 -0300

    feat: logica de boton de modo oscuro

--- front-utnials/src/app/shared/navbar/navbar.ts
+++ front-utnials/src/app/shared/navbar/navbar.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, effect } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { AuthService } from '../../auth/auth-service';
 
 @Component({
@@ -9,4 +10,47 @@ import { AuthService } from '../../auth/auth-service';
 })
 export class Navbar {
   auth = inject(AuthService);
+  doc = inject(DOCUMENT); 
+  modoOscuro: boolean = false;
+
+  constructor() {
+    effect(() => {
+      const usuario = this.auth.usuarioActual();
+      
+      if (usuario) {
+        const temaGuardado = localStorage.getItem('tema');
+        if (temaGuardado === 'dark') {
+          this.modoOscuro = true;
+          this.doc.body.classList.add('dark-theme');
+        } else {
+          this.modoOscuro = false;
+          this.doc.body.classList.remove('dark-theme');
+        }
+      } else {
+        this.modoOscuro = false;
+        this.doc.body.classList.remove('dark-theme');
+      }
+    });
+  }
+
+  toggleDarkMode() {
+    if (!this.auth.usuarioActual()) return;
+
+    this.modoOscuro = !this.modoOscuro;
+    
+    if (this.modoOscuro) {
+      this.doc.body.classList.add('dark-theme');
+      localStorage.setItem('tema', 'dark');
+    } else {
+      this.doc.body.classList.remove('dark-theme');
+      localStorage.setItem('tema', 'light');
+    }
+  }
+
+  cerrarSesionLimpiandoTema() {
+    this.modoOscuro = false;
+    this.doc.body.classList.remove('dark-theme');
+    localStorage.removeItem('tema');
+    this.auth.logout();
+  }
 }
