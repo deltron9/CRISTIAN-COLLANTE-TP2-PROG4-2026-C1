77bce9f6081873d7516517de654736c9950e6cce
deltron9 <cristian_collante@hotmail.com>
2026-06-21 04:08:14 -0300

    add: logica de carga de datos y publis

--- front-utnials/src/app/pages/mi-perfil/mi-perfil.ts
+++ front-utnials/src/app/pages/mi-perfil/mi-perfil.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicacionesService } from '../../services/publicaciones-service/publicaciones-service';
+import { AuthService } from '../../auth/auth-service';
+import { PublicacionesCard } from '../../componentes/publicaciones-card/publicaciones-card';
+import { IPublicacion } from '../../services/publicaciones-service/ipublicacion';
 
 @Component({
-  selector: 'app-mi-perfil',
-  imports: [],
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [CommonModule, PublicacionesCard],
   templateUrl: './mi-perfil.html',
-  styleUrl: './mi-perfil.css',
+  styleUrl: './mi-perfil.css'
 })
-export class MiPerfil {}
+export class Perfil implements OnInit {
+  publis = inject(PublicacionesService);
+  auth = inject(AuthService);
+
+  usuarioLogueado = this.auth.usuarioActual;
+  misPublicaciones = signal<IPublicacion[]>([]);
+
+  ngOnInit(): void {
+    const usuario = this.usuarioLogueado();
+    if (usuario && usuario._id) {
+      this.cargarMisUltimasPublicaciones(usuario._id);
+    }
+  }
+
+  cargarMisUltimasPublicaciones(userId: string): void {
+    this.publis.listar(3, 0, 'createdAt', userId).subscribe(res => this.misPublicaciones.set(res));
+  }
+}
