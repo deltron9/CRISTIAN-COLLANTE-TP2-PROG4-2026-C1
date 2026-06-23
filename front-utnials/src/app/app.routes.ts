a6c28b72aae44a0abaaadd4bed9b6b21b1e4e22c
deltron9 <cristian_collante@hotmail.com>
2026-06-23 16:01:29 -0300

    feat: ruta de spinner, y titulos independientes

--- front-utnials/src/app/app.routes.ts
+++ front-utnials/src/app/app.routes.ts
@@ -16,28 +16,35 @@ export const routes: Routes = [
             {
                 path: 'login',
                 component: Login,
-                canActivate: [RutasParaLogeadosGuard]
+                canActivate: [RutasParaLogeadosGuard],
+                title: 'Utnials | Login'
             },
             {
                 path: 'register',
                 component: Register,
-                canActivate: [RutasParaLogeadosGuard]
+                canActivate: [RutasParaLogeadosGuard],
+                title: 'Utnials | Register'
             }
         ]
     },
     {
+        path: 'pantalla-cargando',
+        loadComponent: () => import('./pages/pantalla-cargando/pantalla-cargando').then((a) => a.PantallaCargando), title: 'Utnials | Verificando Sesión',
+        canActivate: [RutasParaLogeadosGuard]
+    },
+    {
         path: 'mi-perfil',
-        loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((a) => a.MiPerfil),
+        loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((a) => a.MiPerfil), title: 'Utnials | mi perfil',
         canActivate: [RutasParaLogeadosGuard]
     },
     {
         path: 'publicaciones',
-        loadComponent: () => import('./pages/publicaciones/publicaciones').then((a) => a.Publicaciones),
+        loadComponent: () => import('./pages/publicaciones/publicaciones').then((a) => a.Publicaciones), title: 'Utnials | publicaciones',
         canActivate: [RutasParaLogeadosGuard]
     },
     {
         path: 'error-page',
-        component: ErrorPage
+        component: ErrorPage, title: 'Utnials | Error'
     },
     {
         path: '**',
