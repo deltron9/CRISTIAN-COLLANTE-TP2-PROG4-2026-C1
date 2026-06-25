734f2d11273317cfad84904b89c8213ed2ad31b5
deltron9 <cristian_collante@hotmail.com>
2026-06-25 20:23:32 -0300

    fix pipe admin

--- front-utnials/src/app/shared/pipes/rol-color-pipe.ts
+++ front-utnials/src/app/shared/pipes/rol-color-pipe.ts
@@ -1,21 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'rolColor',
   standalone: true
 })
 export class RolColorPipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {}
 
-  transform(nombre: string | null | undefined, idAutor: string | null | undefined, usuarioLogueado: any): SafeHtml | string {
+  transform(nombre: string | null | undefined, idAutor: string | null | undefined, usuarioLogueado: any): string {
     if (!nombre) return '';
     
     if (idAutor && usuarioLogueado && idAutor === usuarioLogueado._id && usuarioLogueado.perfil === 'admin') {
-      const htmlConIcono = `${nombre} <i class="ri-shield-user-fill"></i>`;
-      return this.sanitizer.bypassSecurityTrustHtml(htmlConIcono);
+      return `${nombre} (Admin)`;
     }
 
     return nombre;
   }
+
 }
