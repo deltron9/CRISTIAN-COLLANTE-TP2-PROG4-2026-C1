f682aadc50090efab91ae1d945b127ba07f976b9
deltron9 <cristian_collante@hotmail.com>
2026-06-25 14:39:10 -0300

    chore: rutas para admin con guard y lazy

--- front-utnials/src/app/app.routes.ts
+++ front-utnials/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Register } from './auth/register/register';
 import { Login } from './auth/login/login';
 import { ErrorPage } from './pages/error-page/error-page';
 import { RutasParaLogeadosGuard } from './auth/guards/rutas-para-logeados-guard';
-
+import { adminGuard } from './auth/guards/admin-guard';
 export const routes: Routes = [
     {
         path: '',
@@ -29,22 +29,42 @@ export const routes: Routes = [
     },
     {
         path: 'pantalla-cargando',
-        loadComponent: () => import('./pages/pantalla-cargando/pantalla-cargando').then((a) => a.PantallaCargando), title: 'Utnials | Verificando Sesión',
+        loadComponent: () => import('./pages/pantalla-cargando/pantalla-cargando').then((a) => a.PantallaCargando), 
+        title: 'Utnials | Verificando Sesión',
         canActivate: [RutasParaLogeadosGuard]
     },
     {
         path: 'mi-perfil',
-        loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((a) => a.MiPerfil), title: 'Utnials | mi perfil',
+        loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((a) => a.MiPerfil), 
+        title: 'Utnials | Mi Perfil',
         canActivate: [RutasParaLogeadosGuard]
     },
     {
         path: 'publicaciones',
-        loadComponent: () => import('./pages/publicaciones/publicaciones').then((a) => a.Publicaciones), title: 'Utnials | publicaciones',
+        loadComponent: () => import('./pages/publicaciones/publicaciones').then((a) => a.Publicaciones), 
+        title: 'Utnials | Publicaciones',
         canActivate: [RutasParaLogeadosGuard]
     },
     {
+        path: 'dashboard',
+        canActivate: [adminGuard],
+        children: [
+            {
+                path: 'usuarios',
+                loadComponent: () => import('./pages/dashboard/usuarios/usuarios').then((a) => a.Usuarios),
+                title: 'Utnials | Dashboard - Usuarios'
+            },
+            {
+                path: 'estadisticas',
+                loadComponent: () => import('./pages/dashboard/estadisticas/estadisticas').then((a) => a.Estadisticas),
+                title: 'Utnials | Dashboard - Estadísticas'
+            }
+        ]
+    },
+    {
         path: 'error-page',
-        component: ErrorPage, title: 'Utnials | Error'
+        component: ErrorPage, 
+        title: 'Utnials | Error'
     },
     {
         path: '**',
