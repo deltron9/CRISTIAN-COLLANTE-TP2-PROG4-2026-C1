5c2fe9321ce0f8d80b45ffb83579e8fc0f6368d9
deltron9 <cristian_collante@hotmail.com>
2026-06-25 17:10:33 -0300

    add: servicio para peticiones de estadisticas

---
+++ front-utnials/src/app/services/estadisticas-service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs/internal/Observable';
+
+interface DataEstadistica {
+  label: string;
+  data: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EstadisticasService {
+
+  urlBack = environment.NG_APP_BACKEND_URL;
+  http = inject(HttpClient);
+
+  getPublicacionesPorUsuario(dias: number): Observable<DataEstadistica[]> {
+    return this.http.get<DataEstadistica[]>(`${this.urlBack}/publicaciones/estadisticas/usuarios?dias=${dias}`);
+  }
+
+  getComentariosTimeline(dias: number): Observable<DataEstadistica[]> {
+    return this.http.get<DataEstadistica[]>(`${this.urlBack}/comentarios/estadisticas/timeline?dias=${dias}`);
+  }
+
+  getComentariosPorPublicacion(dias: number): Observable<DataEstadistica[]> {
+    return this.http.get<DataEstadistica[]>(`${this.urlBack}/comentarios/estadisticas/por-publicacion?dias=${dias}`);
+  }
+}
